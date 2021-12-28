0351fc19ff221d0fdd0d4b018e8292427651085d
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 18:26:28 -0500

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,57 @@
-const add = function() {
+const add = function(...num) {
+  let calcSum = 0;
+  for (i = 0; i < num.length; i++){
+    calcSum += num[i];
+    };
+  return calcSum;
+}
+
+const subtract = function(...num) {
+	let calcSum = num[0];
+  for (i = 1; i < num.length; i++){
+    calcSum -= num[i];
+  }
+  return calcSum;
+};
+
+const sum = function(arraySum) {
+  if(arraySum.length == 0){
+    return 0;
+  }if(arraySum.length > 0){
+    let calcSum = 0;
+    for (i = 0; i < arraySum.length; i++){
+      calcSum += arraySum[i];
+      };
+    return calcSum;
+  }
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const multiply = function(num) {
+  let calcSum = num[0];
+  for (i = 1; i < num.length; i++){
+    calcSum *= num[i];
+    };
+  return calcSum;
 
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	powerOutput = x**y;
+  return powerOutput;
 };
 
-const factorial = function() {
+const factorial = function(x) {
+  let calcSum = 1;
+  if(x===0){
+    calcSum = 1;
+  } else {
+    for (i=1; i<=x; i++){
+      calcSum *= i;
+    };
+  }
+
+  return calcSum;
 	
 };
 
