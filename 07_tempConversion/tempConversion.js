e0c0e9a2387de5634c09ceae28667d9a6da144dd
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 13:13:30 -0500

    complete lesson 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(farenheit) {
+  let celcius = (farenheit - 32) * (5/9);
+  let rounded = Math.round(celcius * 10)/10
+  return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  let farenheit = (celcius * (9/5) + 32);
+  let rounded = Math.round(farenheit * 10)/10
+  return rounded;
 };
 
 // Do not edit below this line
