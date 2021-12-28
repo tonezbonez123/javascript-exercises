39b4d9e4e04f742275e2b9e527c7ff394aae6427
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 11:27:39 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
 const reverseString = function(str) {
-
+    let array = str.split(''); //string to array for every character
+    let reversed = array.reverse()//reverse the array
+    
+    //array to string
+    let newString = '';
+    for (i = 0; i < str.length; i++){
+        newString += reversed[i];
+    }
+    return newString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
