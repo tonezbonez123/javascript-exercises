3c53a9ed7db6e7338deea3625ed695ac27be7263
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 09:33:03 -0500

    Complete exercise 2 on repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+function repeatString(phrase, num) {
+    let phraseInside = phrase;
+    let output = '';
+    if (num < 0){
+        output += 'ERROR'
+    } else {   
+        for (i = 0; i < num; i++){
+            output += phraseInside;
+        }
+    }
+    return output
+
 
 };
 
