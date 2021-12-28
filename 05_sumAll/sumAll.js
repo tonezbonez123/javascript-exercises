ead977fc2427b78835b66c0b3fdac322cea56918
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 12:48:11 -0500

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(x,y) {
+    let sum = 0;
+    if ((y<0)||(x<0)||(!Number.isInteger(x))||(!Number.isInteger(y))){
+        sum = 'ERROR';
+    } else if (y>x) {
+        for (i = x; i <= y; i++){
+            sum += i;
+        }
+    } else {
+        for (i = y; i<= x; i++){
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
