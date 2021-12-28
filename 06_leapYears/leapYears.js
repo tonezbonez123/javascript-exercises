2a16fe1dabf71e8e1853aa8e9cd214a2763ac13c
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 12:58:05 -0500

    complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let output = false;
+    if(((year%4 === 0)&&(year%100 != 0))||(year%400 === 0)){
+        output = true;
+    };
+    return output;
 };
 
 // Do not edit below this line
