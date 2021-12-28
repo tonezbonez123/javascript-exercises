4fb61b1208fc15597d4b6b7b581df19483aee80a
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 12:33:37 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+function removeFromArray(listArray, ...num) {
+    for (j=0; j<num.length; j++){
+        let checkNum = num[j]        
+        for (i=0; i<listArray.length; i++){
+            if(listArray[i] === checkNum){
+                listArray.splice(i, 1);
+            } else{
+                continue;
+            }
+        }
+    }
+    return listArray;
 };
 
 // Do not edit below this line
