63e2a43384277c4538d7d3841cca6b832487c2df
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 19:21:16 -0500

    Complete lesson 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let newString = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""); // remove punctuation
+    let finalString = newString.replace(/\s+/g, '');
+    const palindromeArray = finalString.split(''); // create an array to check to see if the index === -(index+1)
+    const reversed = palindromeArray.slice().reverse();
+    let outputStringOne = palindromeArray.join("");
+    let outputStringTwo = reversed.join("");
+    console.log(outputStringOne);
+    console.log(outputStringTwo);
+    return (outputStringOne == outputStringTwo);
+    
+    
+    
 
 };
 
