31e56cdd210fda9abb0e7f3ae7fd8adb24ec895c
tonezbonez123 <anthonynhamilton@gmail.com>
2021-12-28 20:01:37 -0500

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let converted = parseInt(num)
+    let fibArray = [1,1];//create an array of fib numbers
+    let output = 'OOPS';
+    if (converted>0) {
+        for(i=2; i<converted; i++){
+            fibArray.push(fibArray[i-1]+fibArray[i-2]);
+        };
+        output = fibArray[converted-1];
+    }
 
+    return (output);
 };
 
 // Do not edit below this line
